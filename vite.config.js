943bbfd7b4c5bf12dd82b15c9f8bdd08ecbfd3e7
年鉴计划 <tuoni006@163.com>
2026-01-18 00:14:48 +0800

    Build: Update base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
+  base: '/smartisan-icon/',
   plugins: [vue()],
   root: '.',
   publicDir: 'public',
